e8b0d3bd35391550a57fee289de6229835ee5812
Albert <abendayan@itba.edu.ar>
2025-03-15 11:42:18 -0300

    grafico

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import sys
 
+import matplotlib.pyplot as plt
+
 from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect
 
@@ -64,6 +66,37 @@ if __name__ == "__main__":
                 results[ball].append(capture)
                 f.write(f"{capture}\n")
 
+percentages = {}
 
+for ball, captures in results.items():
+    true_count = sum(1 for capture in captures if capture[0] is True)
+    total_count = len(captures)
+    true_percentage = (true_count / total_count) * 100
+    percentages[ball] = true_percentage
+
+balls = list(percentages.keys())
+values = list(percentages.values())
+
+fig, ax = plt.subplots()
+bars = ax.bar(balls, values)
+
+for bar in bars:
+    height = bar.get_height()
+    if(height > -1):
+        ax.text(
+            bar.get_x() + bar.get_width() / 2,  
+            height + 2,  
+            f'{height:.1f}%',  
+            ha='center',  
+            va='bottom',  
+            color='black'  
+        )
+
+plt.xlabel('Pokeballs')
+plt.ylabel('Porcentaje de Captura Exitosa (%)')
+plt.title('Porcentaje de Captura Exitosa por Pokeball')
+plt.ylim(0, 100) 
+
+plt.show()
 
 
