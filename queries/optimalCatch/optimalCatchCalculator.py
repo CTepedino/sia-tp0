dbb7cbd17133636936c4180e5355ee8b6a141393
ctepedino <ctepedino@itba.edu.ar>
2025-03-18 01:47:53 -0300

    OptimalCatchCalculator script

--- queries/optimalCatch/optimalCatchCalculator.py
+++ queries/optimalCatch/optimalCatchCalculator.py
@@ -4,7 +4,6 @@ import sys
 from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect
 
-factory = PokemonFactory("pokemon.json")
 pokeballs = ["pokeball", "ultraball", "fastball", "heavyball"]
 status_effects = [StatusEffect.NONE, StatusEffect.POISON, StatusEffect.BURN, StatusEffect.PARALYSIS, StatusEffect.SLEEP, StatusEffect.FREEZE]
 
@@ -17,49 +16,47 @@ def get_catch_rate(pokemon_name, level, attempts, pokeball, status, hp_percent, 
             captures += 1
     return captures/attempts
 
-def main():
+
+if __name__ == "__main__":
+    factory = PokemonFactory("../../pokemon.json")
+
     with open(f"{sys.argv[1]}", "r") as f:
         config = json.load(f)
 
         pokemon_name = config["pokemon"]
 
         if "level" in config:
-            level = config["level"]
+            level = int(config["level"])
         else:
             level = 100
 
         if "attempts" in config:
             attempts = config["attempts"]
         else:
-            attempts = 1000
+            attempts = 10000
 
         if "noise" in config:
             noise = config["noise"]
         else:
             noise = 0
-
-    if len(sys.argv) >= 3:
-        out_path = sys.argv[3]
+    if len(sys.argv) > 2:
+        out_path = sys.argv[2]
     else:
-        out_path = "./configs/ej2/d/" + pokemon_name + "_optimal_config.json"
+        out_path = f"{pokemon_name}OptimalCatchConfig.json"
 
     optimal_config = {}
     highest_rate = 0
 
     for pokeball in pokeballs:
-        for status in status_effects:
+        for status_effect in status_effects:
             for i in range(1, 100):
-                rate = get_catch_rate(pokemon_name, level, attempts, pokeball, status, i/100, noise)
+                rate = get_catch_rate(pokemon_name, level, attempts, pokeball, status_effect, i / 100, noise)
                 if rate > highest_rate:
-                    optimal_config = {"pokeball": pokeball, "status_effect": status.value[0], "hp_percentage": i/100}
+                    optimal_config = {"pokeball": pokeball, "status_effect": status_effect.value[0], "hp_percentage": i / 100}
                     highest_rate = rate
 
-    optimal_config["highest_rate"] = highest_rate
-    optimal_config["pokemon"] = pokemon_name
-    optimal_config["level"] = level
-    with open(out_path, "w") as f:
-        json.dump(optimal_config, f, ensure_ascii=False, indent=4)
-
-
-if __name__ == "__main__":
-    main()
+            optimal_config["highest_rate"] = highest_rate
+            optimal_config["pokemon"] = pokemon_name
+            optimal_config["level"] = level
+            with open(out_path, "w") as f:
+                json.dump(optimal_config, f, ensure_ascii=False, indent=4)
